eb9f08620629015e56c576406a828251f796372a
jose-menon <menon.jose@outlook.com>
2023-11-25 21:39:13 -0300

    envio de rama con actividad 77

---
+++ DataAccess/DataBaseSeeding/IEntitySeeder.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Track3_Squad2.DataAccess.DataBaseSeeding
+{
+    public interface IEntitySeeder
+    {
+        void SeedDatabase(ModelBuilder modelBuilder);
+
+    }
+}
