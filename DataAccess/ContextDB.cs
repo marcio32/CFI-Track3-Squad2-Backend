c1df030ff1cca8b87e725b7b51cdba14c4f25962
jose-menon <menon.jose@outlook.com>
2023-11-29 18:54:35 -0300

    alta cambios

---
+++ DataAccess/ContextDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Track3_Squad2.DataAccess.DataBaseSeeding;
+using Track3_Squad2.Entities;
+
+namespace Track3_Squad2.DataAccess
+{
+    public class ContextDB : DbContext
+    {
+        public ContextDB(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var seeders = new List<IEntitySeeder>
+            {
+                new AccountSeeder(),
+            };
+
+            foreach (var seeder in seeders) 
+            {
+                seeder.SeedDatabase(modelBuilder);
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
